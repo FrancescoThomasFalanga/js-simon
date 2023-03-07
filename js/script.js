740b3e2f2758abd48e5efdfcbf5ecb6509a6519d
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-03-07 14:22:30 +0100

    randomNumber function + showNumbers function

--- js/script.js
+++ js/script.js
@@ -11,27 +11,90 @@ quali dei numeri da indovinare sono stati individuati.
 
 
 
-
+// campo per visualizzare tutti i numeri random generati
 let showRandomNumber = document.getElementById("show-random-number");
 
+let showResultButtonEl = document.getElementById("show-result-button");
+
+// creo array che conterrà i numeri random
+let haveIt = [];
+// secondi prima che i numeri despawnino
+let secondsLeft = 10;
 
 
-let timeleft = 10;
+// mostra i numeri casuali in PAGINA
+showNumbers();
 
-let countdown = setInterval(count, 10000);
 
-function count() {
+// dopo un countdown di 10 secondi elimina i numeri in PAGINA
+const countdown = setInterval(
 
-    showRandomNumber.innerText = secondsLeft;
+    () => {
+
+        if (secondsLeft <= 0) clearInterval(countdown);
+        showRandomNumber.value = secondsLeft;
+        showRandomNumber.textContent = "";
+        secondsLeft -= 1;
+
+    }, 
+10000)
+
+
+showResultButtonEl.addEventListener("click", function() {
+
+    let firstNumberEl = document.getElementById("first-number").value;
     
+    console.log(firstNumberEl);
+
+    if(haveIt.includes(parseInt(firstNumberEl))) {
         
-    if(secondsLeft == 0) {
-        testoElement.innerText = "AUGURI";
-        clearInterval(capodannInterval);
+        console.log("hai vinto");
+
     }
-        
-    secondsLeft--;
+
+})
+
+
+
+
+
+
+// numero random del computer
+function randomNumberUnique(maxNr) {
+
+    // genero numero random con parametro che darà in base alla difficoltà escluso lo 0
+    let random = Math.floor(Math.random() * maxNr) + 1;
+    
+    // se non include tale numero, allora pushalo dentro l'array
+    if(!haveIt.includes(random)) {
+
+        haveIt.push(random);
+        return random;
+
+    // altrimenti restarta la funzione dall'inizio fin quando non si arriva alla lunghezza massima dell'array = 16
+    } else {
+            
+        if(haveIt.length < maxNr) {
+    
+            return  randomNumberUnique(maxNr);
+
+        }
+    }
+
 }
 
 
+// funzione per mostrare i numeri RANDOM in pagina
+function showNumbers() {
 
+    for (i = 0; i < 5; i++) {
+
+        randomNumberUnique(100);
+    
+    }
+    
+    showRandomNumber.innerText = `${haveIt[0]}  -  ${haveIt[1]}  -  ${haveIt[2]}  -  ${haveIt[3]}  -  ${haveIt[4]}`;
+    
+    console.log(haveIt);
+
+}
