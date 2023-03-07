f561b0381e401cf524892193d95a98889c5fa24b
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-03-07 13:23:08 +0100

    countdown function create

--- js/script.js
+++ js/script.js
@@ -7,3 +7,31 @@ uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e 
 quali dei numeri da indovinare sono stati individuati.
 */
+
+
+
+
+
+let showRandomNumber = document.getElementById("show-random-number");
+
+
+
+let timeleft = 10;
+
+let countdown = setInterval(count, 10000);
+
+function count() {
+
+    showRandomNumber.innerText = secondsLeft;
+    
+        
+    if(secondsLeft == 0) {
+        testoElement.innerText = "AUGURI";
+        clearInterval(capodannInterval);
+    }
+        
+    secondsLeft--;
+}
+
+
+
